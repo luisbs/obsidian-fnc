a2131a28fddf8531af0b9a04c1ea0dc09e441693
Luis Brayan <luis.bs.9004@gmail.com>
2024-06-16 10:22:56 -0600

    refactor: improved implementation of logging

--- src/common/logging.ts
+++ src/common/logging.ts
@@ -1,104 +1,177 @@
 export type LogLevelValue = 0b00 | 0b01 | 0b10 | 0b11
-export const LogLevel: Record<string, LogLevelValue> = {
-  DEBUG: 0b00,
-  INFO: 0b01,
-  WARN: 0b10,
-  ERROR: 0b11,
+export const LogLevel = {
+  ERROR: 0b00 as LogLevelValue,
+  WARN: 0b01 as LogLevelValue,
+  INFO: 0b10 as LogLevelValue,
+  DEBUG: 0b11 as LogLevelValue,
+}
+export const LogLevelReverse: Record<LogLevelValue, string> = {
+  [0b00]: 'ERROR',
+  [0b01]: 'WARN ',
+  [0b10]: 'INFO ',
+  [0b11]: 'DEBUG',
 }
 
 export interface LogEntry {
+  timestamp: Date
+  namespace: string
   level: LogLevelValue
   data: unknown[]
 }
 
-export class Logger {
-  static make(): Logger {
-    return new Logger()
+export interface LoggerDriver {
+  error(prefix: string, data: unknown[]): void
+  warn(prefix: string, data: unknown[]): void
+  info(prefix: string, data: unknown[]): void
+  debug(prefix: string, data: unknown[]): void
+
+  /** Starts a logs group, and returns a function to end the group. */
+  group(prefix: string, data: unknown[]): () => void
+}
+
+export class ConsoleLoggerDriver implements LoggerDriver {
+  public error(prefix: string, data: unknown[]): void {
+    console.error(prefix, ...data)
+  }
+  public warn(prefix: string, data: unknown[]): void {
+    console.warn(prefix, ...data)
+  }
+  public info(prefix: string, data: unknown[]): void {
+    console.log(prefix, ...data)
+  }
+  public debug(prefix: string, data: unknown[]): void {
+    console.debug(prefix, ...data)
   }
 
-  static formatTimestamp(timestamp: Date): string {
-    //2021-05-04 13:14:15.012
+  public group(prefix: string, data: unknown[]): () => void {
+    console.groupCollapsed(...data)
+    return console.groupEnd
+  }
+}
+
+export class FileLoggerDriver implements LoggerDriver {
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  public error(prefix: string, data: unknown[]): void {
+    throw new Error('FileLogger not implemented')
+  }
+  public warn(prefix: string, data: unknown[]): void {
+    throw new Error('FileLogger not implemented')
+  }
+  public info(prefix: string, data: unknown[]): void {
+    throw new Error('FileLogger not implemented')
+  }
+  public debug(prefix: string, data: unknown[]): void {
+    throw new Error('FileLogger not implemented')
+  }
+
+  public group(prefix: string, data: unknown[]): () => void {
+    throw new Error('FileLogger not implemented')
+  }
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+}
+
+export class Logger {
+  // TODO: add support for file logging
+  protected static drivers: LoggerDriver[] = [new ConsoleLoggerDriver()]
+  // public static setDrivers(): void {}
+
+  constructor(protected namespace: string) {}
+
+  protected static prefix(timestamp: Date, namespace: string, level: LogLevelValue): string {
+    // [13:14:15.012] LEVEL (namespace): <message>
+    //`[${hh}:${mm}:${ss}.${ms}] ${lvl} (${namespace}):`
+    // prettier-ignore
     return (
-      //timestamp.getFullYear() +
-      //'-' +
-      //String(timestamp.getMonth() + 1).padStart(2, '0') +
-      //'-' +
-      //String(timestamp.getDate()).padStart(2, '0') +
-      //' ' +
-      String(timestamp.getHours()).padStart(2, '0') +
-      ':' +
-      String(timestamp.getMinutes()).padStart(2, '0') +
-      ':' +
-      String(timestamp.getSeconds()).padStart(2, '0') +
-      '.' +
-      String(timestamp.getMilliseconds()).padStart(3, '0')
+      '[' + String(timestamp.getHours()).padStart(2, '0') +
+      ':' + String(timestamp.getMinutes()).padStart(2, '0') +
+      ':' + String(timestamp.getSeconds()).padStart(2, '0') +
+      '.' + String(timestamp.getMilliseconds()).padStart(3, '0') +
+      '] ' + LogLevelReverse[level] + ' (' + namespace + '):'
     )
   }
 
-  static withTs(data: unknown[]): unknown[] {
-    data.unshift(Logger.formatTimestamp(new Date()))
-    return data
-  }
+  public log(timestamp: Date, namespace: string, level: LogLevelValue, data: unknown[]): void {
+    const prefix = Logger.prefix(timestamp, namespace, level)
 
-  group(...data: unknown[]): LoggingGroup {
-    return LoggingGroup.make(...data)
-  }
-
-  debug(...data: unknown[]): void {
-    console.debug(...Logger.withTs(data))
-  }
-  info(...data: unknown[]): void {
-    console.log(...Logger.withTs(data))
-  }
-  warn(...data: unknown[]): void {
-    console.warn(...Logger.withTs(data))
-  }
-  error(...data: unknown[]): void {
-    console.error(...Logger.withTs(data))
-  }
-
-  log(level: LogLevelValue, data: unknown[]): void {
     switch (level) {
       case LogLevel.ERROR:
-        return console.error(...data)
+        return Logger.drivers.forEach((driver) => driver.error(prefix, data))
       case LogLevel.WARN:
-        return console.warn(...data)
+        return Logger.drivers.forEach((driver) => driver.warn(prefix, data))
       case LogLevel.INFO:
-        return console.log(...data)
+        return Logger.drivers.forEach((driver) => driver.info(prefix, data))
       default:
-        return console.debug(...data)
+        return Logger.drivers.forEach((driver) => driver.debug(prefix, data))
     }
+  }
+
+  public error(...data: unknown[]): void {
+    this.log(new Date(), this.namespace, LogLevel.ERROR, data)
+  }
+  public warn(...data: unknown[]): void {
+    this.log(new Date(), this.namespace, LogLevel.WARN, data)
+  }
+  public info(...data: unknown[]): void {
+    this.log(new Date(), this.namespace, LogLevel.INFO, data)
+  }
+  public debug(...data: unknown[]): void {
+    this.log(new Date(), this.namespace, LogLevel.DEBUG, data)
+  }
+
+  public on(logger: unknown): Logger {
+    return logger && logger instanceof Logger ? new LoggerWrapper(this.namespace, logger) : this
+  }
+
+  public group(...data: unknown[]): LoggingGroup {
+    return new LoggingGroup(this.namespace, ...data)
+  }
+}
+
+export class LoggerWrapper extends Logger {
+  constructor(namespace: string, private logger: Logger) {
+    super(namespace)
+  }
+
+  public error(...data: unknown[]): void {
+    this.logger.log(new Date(), this.namespace, LogLevel.ERROR, data)
+  }
+  public warn(...data: unknown[]): void {
+    this.logger.log(new Date(), this.namespace, LogLevel.WARN, data)
+  }
+  public info(...data: unknown[]): void {
+    this.logger.log(new Date(), this.namespace, LogLevel.INFO, data)
+  }
+  public debug(...data: unknown[]): void {
+    this.logger.log(new Date(), this.namespace, LogLevel.DEBUG, data)
   }
 }
 
 export class LoggingGroup extends Logger {
   #logs: LogEntry[] = []
 
-  static make(...data: unknown[]): LoggingGroup {
-    const self = new LoggingGroup()
-    self.info(...data)
-    return self
+  constructor(namespace: string, ...data: unknown[]) {
+    super(namespace)
+    this.info(...data)
   }
 
-  debug(...data: unknown[]): void {
-    this.#logs.push({ level: LogLevel.DEBUG, data: Logger.withTs(data) })
-  }
-  info(...data: unknown[]): void {
-    this.#logs.push({ level: LogLevel.INFO, data: Logger.withTs(data) })
-  }
-  warn(...data: unknown[]): void {
-    this.#logs.push({ level: LogLevel.WARN, data: Logger.withTs(data) })
-  }
-  error(...data: unknown[]): void {
-    this.#logs.push({ level: LogLevel.ERROR, data: Logger.withTs(data) })
+  public log(timestamp: Date, namespace: string, level: LogLevelValue, data: unknown[]): void {
+    this.#logs.push({ timestamp, namespace, level, data })
   }
 
   flush(...data: unknown[]): void {
-    console.groupCollapsed(...Logger.withTs(data))
-    for (const entry of this.#logs) {
-      this.log(entry.level, entry.data)
+    // TODO: add support for file logging
+    const prefix = Logger.prefix(new Date(), this.namespace, LogLevel.INFO)
+
+    // starts logging groups
+    const closers = Logger.drivers.map((driver) => driver.group(prefix, data))
+
+    // logs stored entries
+    for (const { timestamp, namespace, level, data } of this.#logs) {
+      super.log(timestamp, namespace, level, data)
     }
-    console.groupEnd()
+
+    // ends logging groups
+    for (const closer of closers) closer()
 
     this.#logs = []
   }
